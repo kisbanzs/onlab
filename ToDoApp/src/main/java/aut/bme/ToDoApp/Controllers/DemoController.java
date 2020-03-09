7d26ea22be33262d10f3be6ed7cce4d207a06fc6
Zsofi <zsofi.kisban@gmail.com>
2020-03-09 12:23:42 +0100

    Kezdetleges kinezet

--- ToDoApp/src/main/java/aut/bme/ToDoApp/Controllers/DemoController.java
+++ ToDoApp/src/main/java/aut/bme/ToDoApp/Controllers/DemoController.java
@@ -1,9 +1,11 @@
 package aut.bme.ToDoApp.Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class DemoController {
 
